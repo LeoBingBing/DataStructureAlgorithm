2d1620ddc2a22e3ef69e992fd01296ca016d245f
wb <1079989125@qq.com>
2018-09-24 14:21:03 +0800

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author wb
+ * @date 2018/9/24
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
